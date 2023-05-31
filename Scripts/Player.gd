caf4d3a05ebaeca2d5f95a550d479575f2c66672
Guilherme Calça <guilhermecalca@hotmail.com>
2023-05-31 00:30:15 -0300

    Animations and Parallax

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,18 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_jumping := false
 
+@onready var animation := $Animation as AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		is_jumping = true
+		animation.play("Jump")
+	else:
+		is_jumping = false
+		
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -22,7 +28,12 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		if !is_jumping:
+			animation.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if !is_jumping:
+			animation.play("Idle")
 
 	move_and_slide()
